f3c6783d63c4d733efad1b97d26a64eec9d82017
kristiyan.velkov <kristiyan.velkov@ffw.com>
2024-08-08 16:24:00 +0300

    [feat] added my first component demo and update the watch script to work and update the browser

--- ui.frontend/webpack.dev.js
+++ ui.frontend/webpack.dev.js
@@ -1,42 +1,45 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const SOURCE_ROOT = __dirname + '/src/main/webpack';
+const SOURCE_ROOT = __dirname + "/src/main/webpack";
 
-module.exports = env => {
+module.exports = (env) => {
+  const writeToDisk = env && Boolean(env.writeToDisk);
 
-    const writeToDisk = env && Boolean(env.writeToDisk);
-
-    return merge(common, {
-        mode: 'development',
-        performance: {
-            hints: 'warning',
-            maxAssetSize: 1048576,
-            maxEntrypointSize: 1048576
+  return merge(common, {
+    mode: "development",
+    performance: {
+      hints: "warning",
+      maxAssetSize: 1048576,
+      maxEntrypointSize: 1048576,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, SOURCE_ROOT + "/static/index.html"),
+      }),
+    ],
+    devServer: {
+      proxy: [
+        {
+          context: ["/content", "/etc.clientlibs", "/libs"],
+          target: "http://localhost:4502",
         },
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: path.resolve(__dirname, SOURCE_ROOT + '/static/index.html')
-            })
-        ],
-        devServer: {
-            proxy: [{
-                context: ['/content', '/etc.clientlibs', '/libs'],
-                target: 'http://localhost:4502',
-            }],
-            client: {
-                overlay: {
-                    errors: true,
-                    warnings: false,
-                },
-            },
-            watchFiles: ['src/**/*'],
-            hot: false,
-            devMiddleware: {
-                writeToDisk: writeToDisk
-            }
-        }
-    });
-}
+      ],
+      client: {
+        overlay: {
+          errors: true,
+          warnings: false,
+        },
+        progress: true, // Show compilation progress in the browser console
+        reconnect: true, // Reconnects to dev server when the server is restarted
+      },
+      watchFiles: ["src/**/*"],
+      hot: true,
+      devMiddleware: {
+        writeToDisk: writeToDisk,
+      },
+    },
+  });
+};
